91048cae3f9ea9a94d995bf5b0d9d0a779df59c1
Guang Han <sniperstriker@gmail.com>
2023-08-22 00:13:54 +0200

    update a blog

---
+++ content/blog/数学天地/hypothesis test.md
@@ -0,0 +1,58 @@
+Title: 线性回归的假设检验
+Date: 2023-08-21
+Tags: 数学
+
+三种假设检验：lagrange multiplyer test, likelihood-ratio test 与 wald test
+
+## 线性回归的检验
+
+### 模型
+
+假设回归的模型为$Y=\alpha + \beta x + \epsilon$
+
+0假设为x于y是uncorrelated，进行回归之后，$\hat{\alpha}$和$\hat{\beta}$作为截距和slope被计算出来，$SE_{\hat{\beta}}$是各个点对于line的standard error。之后计算$\hat{\beta}$的t值：
+
+![Untitled]({attach}ht-formula1.png)
+
+服从自由度为n-2的t-distribution，
+
+其中slope coefficent的standard error是
+
+![Untitled]({attach}ht-formula2.png)
+
+就是用y的偏差值（自由度为n-2）用x的方差进行归一化
+
+样本的标准差被计作s，就是根号下除以n-1的那个，这样计算出来的样本标准差是整体标准差的无偏估计，也就是$E(s)=\sigma$，这个操作叫作bessel‘s correction
+
+### bessel’s correction
+
+$$
+\sum_{i = 1}^n (x_i - \bar{x}) ^ 2 = \sum (x_i^2 - 2x_i\bar{x} + \bar{x} ^2) = (\sum x_i^2) - 2n\bar{x}^2 + n\bar{x}^2 = \sum x_i^2 - n\bar{x}^2
+$$
+
+$$
+E(☝) = \sum E(x_i^2) - n E(\bar{x}^2) = \sum (var(x_i) + E(x_i)^2) - n (var(\bar{x}) + E(\bar{x})^2)
+$$
+
+其中，
+
+$$
+var(x_i) = \sigma, E(x_i) = \mu, E(\bar{x}) = \mu, var(\bar{x}) = \sigma/n
+$$
+
+所以上边的等于$\sum_{(1..n)} (\sigma + \mu^2) - \sigma - n \mu^2$
+
+两个$n\mu^2$消去，剩下一个$(n-1)\sigma$.
+
+所以$\sum_{i = 1}^n (x_i - \bar{x}) ^ 2$要除以n-1才是$\sigma$的无偏估计
+
+### 假设检验，以t-test为例
+
+假设检验的目的：
+
+- 双样本检验：检测两个随机变量是不同的，它先从两个随机变量里面各自抽取一些样本，然后对这些样本进行假设检验，这就是two-sample t-test，比如随机分组100个病人到治疗和对照组里面。
+- 单样本检验：student-t这个统计量就是把观测量进行“正规化”，也就是用样本均值减去整体均值，除以（样本方差/根号n，n是样本个数）。分母这部分又叫作mean的standard error，是一个statistic的样本的标准差除以根号n。它假设$\bar{x}$也就是样本均值服从以$\mu$为均值，$\sigma^2/n$为方差的normal distribution，检验这个t统计量是否$～\mathcal{N}(0, 1)$
+
+### Wald test
+
+未完待续...
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,25 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 import math
+
 JINJA_FILTERS = {
-    'count_to_font_size': lambda c: '{p:.1f}%'.format(p=100 + 25 * math.log(c, 2)),
+    "count_to_font_size": lambda c: "{p:.1f}%".format(p=100 + 25 * math.log(c, 2)),
 }
 
-AUTHOR = u'\u97e9\u5149'
-SITENAME = u'\u7b80\u5e90'
-SITEURL = 'http://joshuaghost.github.io'
+AUTHOR = "Zhang, Zijian"
+SITENAME = "Zijian's Hunting Cabin"
+SITEURL = "joshuaghost.github.io"
 
-PATH = 'content'
-STATIC_PATHS = ['images', 'pdfs', 'blog', 'pages', 'extra']
-ARTICLE_PATHS = ['blog']
-ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
-ARTICLE_URL = '{date:%Y}/{slug}.html'
+PATH = "content"
+STATIC_PATHS = ["images", "pdfs", "blog", "pages", "extra"]
+ARTICLE_PATHS = ["blog"]
+ARTICLE_SAVE_AS = "{date:%Y}/{slug}.html"
+ARTICLE_URL = "{date:%Y}/{slug}.html"
 
-TIMEZONE = 'Europe/Berlin'
+TIMEZONE = "Europe/Berlin"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -28,29 +25,41 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# 友情链接 
-LINKS = (('宋壬初', 'http://renchusong.github.io/portfolio/'),
-         ('Wrfly\'s blog','http://wrfly.kfd.me/'),
-         ('Avishek\'s Homepage', 'https://www.l3s.de/~anand/'),
-         ('Jaspreet\'s Homepage', 'http://www.l3s.de/~singh/'),)
+# Blogroll
+LINKS = (
+    ("宋壬初", "http://renchusong.github.io/portfolio/"),
+    ("Wrfly's blog", "http://wrfly.kfd.me/"),
+    ("Avishek's Homepage", "https://www.l3s.de/~anand/"),
+    ("Jaspreet's Homepage", "http://www.l3s.de/~singh/"),
+    ("科学空间", "https://spaces.ac.cn/"),
+)
 
 # Social widget
-SOCIAL = (('Fork me on GitHub!', 'http://github.com/JoshuaGhost'),
-          ('Twitt me!','http://twitter.com/Joshua_Ghost/'),
-          ('Mail me!','mailto:zzhang@l3s.de'),
-          ("My medium page", 'https://medium.com/@zhangzijian0523'))
+SOCIAL = (
+    ("GitHub", "http://github.com/JoshuaGhost"),
+    ("Tweet", "http://twitter.com/Joshua_Ghost/"),
+    ("Mail me", "mailto:zhangzijian0523@gmail.com"),
+    ("My Medium page", "https://medium.com/@zhangzijian0523"),
+)
 
-DEFAULT_PAGINATION = 6
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-USE_FOLDER_AS_CATEGORY = True 
+USE_FOLDER_AS_CATEGORY = True
 DISPLAY_PAGES_ON_MENU = True
 
 # Custom settings
+USE_CUSTOM_MENU = True
 SUMMARY_MAX_LENGTH = 25
-THEME = 'theme/foundation-default-colours/'
-PLUGIN_PATHS = ["/home/assassin/workspace/pelican-plugins"]
-PLUGINS = ["liquid_tags"]
-EXTRA_PATH_METADATA = {'extra/favicon.ico':{'path': 'favicon.ico'}}
+THEME = "theme/foundation-default-colours/"
+SITEDESCRIPTION = "Zijian's personal blog"
+CONTACT_EMAIL = "zhangzijian0523@gmail.com"
+PLUGINS = ["liquid_tags", "render_math"]
+EXTRA_PATH_METADATA = {"extra/favicon.ico": {"path": "favicon.ico"}}
+MATH_JAX = {
+    # "color": "blue",
+    "align": "left",
+    "tex_extensions": ["color.js", "mhchem.js"],
+}
